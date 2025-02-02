c77c329470cd1800cca26db7695e7b5f659bfe74
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-02-02 21:59:52 +0500

    add five section

--- scripthomepage.js
+++ scripthomepage.js
@@ -11,3 +11,27 @@
       navbar.classList.remove('hidden');
     }
   });
+
+
+  //=============================================================
+
+  gsap.registerPlugin(ScrollTrigger);
+
+
+  const section10CardsContainer3 = document.getElementById("section10CardsContainer3");
+  
+
+  
+  gsap.to(section10CardsContainer3, {
+    x: () => -(section10CardsContainer3.scrollWidth - window.innerWidth),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#section10CardsWrapper3",
+      start: "top top",
+      end: () => `+=${section10CardsContainer3.scrollWidth}`,
+      pin: true,
+      scrub: 0.5,
+      invalidateOnRefresh: true,
+    },
+  });
+  
